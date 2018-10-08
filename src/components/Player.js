bdec8c2c5b63de5639db79d53b67944bbb8c09ba
Riley-Brown <riley@riley.gg>
2018-10-07 20:44:38 -0400

    Added files

---
+++ src/components/Player.js
@@ -0,0 +1,27 @@
+import React, { PureComponent } from "react";
+import Counter from "./Counter";
+class Player extends PureComponent {
+  render() {
+    return (
+      <div className="player">
+        <span className="player-name">
+          <button
+            className="remove-player"
+            onClick={() => this.props.removePlayer(this.props.id)}
+          >
+            ✖
+          </button>
+          {this.props.name}
+        </span>
+
+        <Counter
+          score={this.props.score}
+          changeScore={this.props.changeScore}
+          index={this.props.index}
+        />
+      </div>
+    );
+  }
+}
+
+export default Player;
